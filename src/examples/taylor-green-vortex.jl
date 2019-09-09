5b30d18b4852a1d08b9d4a88714b293ff38cd924
Mark Redeman <markredeman@gmail.com>
2019-09-09 08:48:07 +0200

    Wip 2

--- src/examples/taylor-green-vortex.jl
+++ src/examples/taylor-green-vortex.jl
@@ -7,6 +7,7 @@ using lbm
 using Plots
 
 struct TaylorGreenVortexExample <: lbm.InitialValueProblem
+    scale
     rho_0
     u_max
     ν
@@ -15,11 +16,12 @@ struct TaylorGreenVortexExample <: lbm.InitialValueProblem
     k_x
     k_y
 
-    function TaylorGreenVortexExample(ν = 1.0 / 6.0 , scale = 2, NX = 32 * scale, NY = NX)
-        u_max = 0.04 / scale
+    function TaylorGreenVortexExample(ν = 1.0 / 6.0 , scale = 2, NX = 16 * scale, NY = NX)
+        u_max = 0.02 / scale
         Re = NX * u_max / ν
         @show Re
         return new(
+            scale,
             1.0,
             u_max,
             ν,
@@ -61,6 +63,141 @@ function decay(tgv::TaylorGreenVortexExample, x, y, timestep::Int)
     exp(-1.0 * timestep / td)
 end
 
+
+function process!(tgv, quadrature, f_in, t, stats)
+        # Density
+        ρ = lbm.density(quadrature, f_in)
+
+        # Momentum
+        j = lbm.momentum(quadrature, f_in)
+        # E = lbm.total_energy(quadrature, f_in)
+        E_k = lbm.kinetic_energy(quadrature, f_in, ρ, j ./ ρ)
+        # ϵ = lbm.internal_energy(quadrature, f_in, ρ, j ./ ρ)
+
+        # E_k = sum((j[:, :, 1].^2 + j[:, :, 1].^2) ./ ρ)
+
+
+        N = size(f_in, 1)
+        density_field = fill(0.0, N, N)
+        velocity_field = fill(0.0, N, N, lbm.dimension(quadrature))
+
+        for x in 1:N, y in 1:N
+            density_field[x, y] = density(tgv, x, y, t)
+            velocity_field[x, y, :] = velocity(tgv, x, y, t)
+        end
+
+        rho_error_squared = sqrt(
+            sum(
+                (ρ - density_field).^2 ./
+                (density_field).^2
+            )
+        )
+        ux_error_squared = sqrt(
+            sum(
+                (ρ .* j[:, :, 1] - velocity_field[:, :, 1]).^2 ./ velocity_field[:, :, 1].^2
+            )
+        )
+        uy_error_squared = sqrt(
+            sum(
+                (ρ .* j[:, :, 2] - velocity_field[:, :, 2]).^2 ./ velocity_field[:, :, 2].^2
+            )
+        )
+        u_error = sqrt(
+            sum(
+            ((ρ .* j[:, :, 2] - velocity_field[:, :, 2]).^2 .+
+             (ρ .* j[:, :, 2] - velocity_field[:, :, 2]).^2) ./
+            (velocity_field[:, :, 1].^2 .+ velocity_field[:, :, 2].^2)
+                )
+        )
+
+
+      # sumrhoe2 += (rho - rhoa) * (rho - rhoa);
+      # sumuxe2 += (ux - uxa) * (ux - uxa);
+      # sumuye2 += (uy - uya) * (uy - uya);
+
+      # sumrhoa2 += (rhoa - rho0) * (rhoa - rho0);
+      # sumuxa2 += uxa * uxa;
+      # sumuya2 += uya * uya;
+
+        # Compare with analytical results?
+        push!(stats, [
+            sum(ρ),
+            sum(j),
+            1.0,
+            sum(E_k),
+            1.0,
+            sum(density_field),
+            sum(velocity_field ./ density_field),
+            1.0,
+            sum(
+                density_field .* (velocity_field[:, :, 1].^2 + velocity_field[:, :, 2].^2)
+            ),
+            1.0,
+            rho_error_squared,
+            ux_error_squared,
+            uy_error_squared,
+            u_error,
+        ])
+
+    return
+        # if (mod(t, 50) == 0)
+        #     plot(
+        #         plot(stats.density),
+        #         plot(stats.momentum),
+        #         # plot(stats.total_energy),
+        #         plot(stats.kinetic_energy),
+        #         plot(stats.density_a),
+        #         plot(stats.momentum_a),
+        #         # plot(stats.total_energy),
+        #         plot(stats.kinetic_energy_a),
+        #         # plot(stats.internal_energy),
+        #         legend=nothing
+        #     )
+
+        #     gui()
+        # end
+
+        u = j[:, :, 1] ./ ρ
+        v = j[:, :, 2] ./ ρ
+
+        s = (1000, 500)
+        plot(
+            contour(ρ[:, :, 1], fill=true, clims=(0, 1.05), cbar=true, size=s),
+            streamline(j),
+            streamline(velocity_field),
+            streamline(velocity_field .- ρ .* j),
+            # velocity_field,
+            plot(stats.u_error, legend=false, title="U_e"),
+            plot(stats.kinetic_energy, legend=false, title="Kinetic energy", size=s),
+            # contour(u[:, :, 1].^2, fill=true, cbar=true, size=s, title="u"),
+            # contour(v[:, :, 1].^2, fill=true, cbar=true, size=s, title="v"),
+            plot(j[:, 4, 1], size=s),
+            plot(j[:, 4, 2], size=s),
+            size=(1000, 600)
+        )
+        gui()
+end
+
+
+function streamline(j; step = round(Int, size(j, 1) / 5) )
+
+    s = (1000, 500)
+        velocity_field = contour(j[:, :, 1].^2 .+ j[:, :, 2].^2, cbar=true, fill=true, title="Momentum", size=s)
+        N = size(j, 1)
+        X = [i for i in range(1, size(j, 1), step = step), j in range(1, size(j, 2), step = step)]
+        Y = [j for i in range(1, size(j, 1), step = step), j in range(1, size(j, 2), step = step)]
+        # @show "process: ", u, v,
+
+        quiver!(
+            velocity_field,
+            X, Y,
+            # quiver=(u[:, :, 1], v[:, :, 1])
+            quiver=(x, y) -> (j[x, y, 1] / sqrt(j[x, y, 1]^2 + j[x, y, 2]^2), j[x, y, 2] / sqrt(j[x, y, 1]^2 + j[x, y, 2]^2)) ,
+            color="white",
+        )
+    return velocity_field
+end
+
 function initialize!(quadrature, tgv, N)
     density_field = fill(0.0, N, N)
     velocity_field = fill(0.0, N, N, lbm.dimension(quadrature))
@@ -93,6 +230,7 @@ function siumlate(tgv::TaylorGreenVortexExample;)
     @show tgv
     N = tgv.NX
     τ = quadrature.speed_of_sound_squared * tgv.ν + 0.5
+    @show τ
     # τ = 0.5;
 
     # initialize
@@ -100,54 +238,105 @@ function siumlate(tgv::TaylorGreenVortexExample;)
     f_in = copy(f_out)
 
     stats = DataFrame(
-        [Float64[], Float64[], Float64[], Float64[], Float64[]],
-        [:density, :momentum, :total_energy, :kinetic_energy, :internal_energy]
+        [
+            Float64[], Float64[], Float64[], Float64[], Float64[],
+            Float64[], Float64[], Float64[], Float64[], Float64[],
+            Float64[], Float64[], Float64[], Float64[]
+        ],
+        [
+            :density, :momentum, :total_energy, :kinetic_energy, :internal_energy,
+            :density_a, :momentum_a, :total_energy_a, :kinetic_energy_a, :internal_energy_a,
+            :density_e, :momentum_e, :kinetic_energy_e, :u_error
+        ]
     )
 
     # return f_in
-    @inbounds for t = 0 : 2N
-        if mod(t, 10) == 0
-            @show t, t / 2N
+    n_steps = 100 * tgv.scale * tgv.scale
+    @inbounds for t = 0 : n_steps
+        if mod(t, round(Int, n_steps / 10)) == 0
+            @show t, t / n_steps
         end
-        process!(quadrature, f_in, t, stats)
+
+        if (mod(t, 1) == 0)
+            # process!(tgv, quadrature, f_in, t, stats)
+        end
 
         f_out = collide(SRT(τ), quadrature, f_in)
 
         f_in = stream(quadrature, f_out)
     end
+    process!(tgv, quadrature, f_in, n_steps + 1, stats)
 
     @show stats
 
 
-    plot(
-        # plot(stats.density),
-        # plot(stats.momentum),
-        plot(stats.total_energy),
-        plot(stats.kinetic_energy),
-        plot(stats.internal_energy),
-        size=(1000, 600)
-    )
+    # plot(
+    #     # plot(stats.density),
+    #     # plot(stats.momentum),
+    #     plot(stats.total_energy),
+    #     plot(stats.kinetic_energy),
+    #     plot(stats.internal_energy),
+    #     size=(1000, 600)
+    # )
 
-    gui()
+            # plot(
+            #     plot(stats.density),
+            #     plot(stats.momentum),
+            #     # plot(stats.total_energy),
+            #     plot(stats.kinetic_energy),
+            #     plot(stats.density_a),
+            #     plot(stats.momentum_a),
+            #     # plot(stats.total_energy),
+            #     plot(stats.kinetic_energy_a),
+            #     plot(stats.u_error),
+            #     # plot(stats.internal_energy),
+            #     legend=nothing
+            # )
+
+    #         plot(
+    #             plot(stats.kinetic_energy),
+    #             plot(stats.kinetic_energy_a),
+    #             plot(stats.density_e),
+    #             plot(stats.momentum_e),
+    #             plot(stats.kinetic_energy_e),
+    #             plot(stats.u_error),
+    #             legend=nothing
+    #         )
+
+    # gui()
 
     f_in, stats
 end
 
-# N = 2^5
-example = TaylorGreenVortexExample(
-    1.0 / 6.0,
-    1,
-)
+stats = DataFrame([Float64[], Int[], Any[]], [:nu, :scale, :stats])
 
-@time result = siumlate(example)
+νs = (0.0:6.0) ./ 6.0
+for ν in νs
+for scale = [1, 2, 4, 8]
+    # N = 2^5
+# scale = 4
+    example = TaylorGreenVortexExample(
+        ν,
+        scale,
+    )
 
-using Test
+    @time result = siumlate(example);
+    push!(stats, [ν, scale, result[2]])
+end
+end
 
-quadratures = [
-    D2Q4(),
-    D2Q5(),
-    D2Q9()
-]
+using Plots
+nu_scale_error = Array{Float64}([s.nu s.scale map(i -> i.u_error[1], s.stats)])
+plot()
+for nu_idx = 0:length(0.0:6.0) - 2
+    nu = nu_scale_error[nu_idx * 5 + 1, 1]
+    plot!(map(i -> 16 * i, nu_scale_error[nu_idx * 5 .+ (2:5), 2]), nu_scale_error[nu_idx * 5 .+ (2:5), 3], label="nu = $nu")
+end
+gui()
+
+nu_idx = 2
+    plot(nu_scale_error[nu_idx * 5 .+ (1:5), 2], nu_scale_error[nu_idx * 5 .+ (1:5), 3])
+
 end
 end
 
