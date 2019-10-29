f019ec9e77eca80e2efc02df21fd19caa759cf74
Mark Redeman <markredeman@gmail.com>
2019-10-29 15:54:48 +0100

    Cleanup poiseuille code

--- src/problems/poiseuille.jl
+++ src/problems/poiseuille.jl
@@ -15,18 +15,16 @@ end
 function PoiseuilleFlow(
     ν = 1.0 / 6.0, scale = 2, NX = 5 * scale + 0, NY = NX, domain_size = (1.0, 1.0) ; static = true
 )
+    # Since our velocity profile is constant over x it is sufficient to only take
+    # 3 nodes in the x direction
+    NX = 3
     u_max = 0.1 / scale
-
-    Re = NY * u_max / ν
-    @show u_max, Re, ν
-
-    # @show (ν - Δt / 2)^2
+   
     return PoiseuilleFlow(
         1.0,
         u_max,
         ν,
-        # NX,
-        5,
+        NX,
         NY,
         1.0,
         domain_size
@@ -60,23 +58,17 @@ function force(problem::PoiseuilleFlow, x::Float64, y::Float64, time::Float64 = 
 end
 
 function delta_x(problem::PoiseuilleFlow)
-    # Don't include the two boundary nodes
-    return problem.domain_size[2] * (1 / (problem.NY) )
+    return problem.domain_size[2] / problem.NY
+end
+
+function reynolds(problem::Poiseuilleflow)
+    ν = problem.ν
+    NY = problem.NY
+    u_max = problem.u_max
+    return Re = NY * u_max / ν
 end
 
 has_external_force(problem::PoiseuilleFlow) = true
-
-# Temporary
-function is_fluid(problem::PoiseuilleFlow, x::Int64, y::Int64)
-    return true
-    if y == 1
-        return false
-    end
-    if y == problem.NY
-        return false
-    end
-    return true
-end
 
 function apply_boundary_conditions_before!(q::Quadrature, problem::PoiseuilleFlow; time = t * Δt, f_new, f_old)
     for k = 1:size(f_old, 3)
@@ -85,31 +77,26 @@ function apply_boundary_conditions_before!(q::Quadrature, problem::PoiseuilleFlo
     end
 end
 
+"""
+Apply a bottom and top wall bounce back boundary condition
+
+The nodes at y = 1, NY are facing a stationary wall
+The bounce back boundary condition will reflect any incoming particles
+"""
 function apply_boundary_conditions_after!(q::Quadrature, problem::PoiseuilleFlow; time = t * Δt, f_new, f_old)
-    for x_idx = 1 : problem.NX
-        # Bottom wall
-        y_idx = 1
-        # for f_idx = 1:size(f_new, 3)
-        for f_idx = 1:size(f_new, 3)
-            if q.abscissae[2, f_idx] > 0
-                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite(q, f_idx)]
-            end
+    # Bottom wall
+    y_idx = 1
+    for f_idx = 1:size(f_new, 3)
+        if q.abscissae[2, f_idx] > 0
+            f_new[:, y_idx, f_idx] = f_old[:, y_idx, opposite(q, f_idx)]
         end
-        # for f_idx = [4, 5, 6]
-        #     f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite(q, f_idx)]
-        # end
+    end
 
-        # Top wall
-        y_idx = problem.NY
-
-        for f_idx = 1:size(f_new, 3)
-            if q.abscissae[2, f_idx] < 0
-                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite(q, f_idx)]
-            end
+    # Top wall
+    y_idx = problem.NY
+    for f_idx = 1:size(f_new, 3)
+        if q.abscissae[2, f_idx] < 0
+            f_new[:, y_idx, f_idx] = f_old[:, y_idx, opposite(q, f_idx)]
         end
-        # for f_idx = 1:size(f_new, 3)
-        # for f_idx = [2, 8, 9]
-        #     f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite(q, f_idx)]
-        # end
     end
 end
