d13d936e67f2047dc5004164623894da82dca8b2
Mark Redeman <markredeman@gmail.com>
2019-11-04 13:54:11 +0100

    Add a function to visualize quadratures

--- src/quadratures/quadrature.jl
+++ src/quadratures/quadrature.jl
@@ -33,3 +33,59 @@ include("D2Q21.jl")
 include("D2Q37.jl")
 include("thermodynamics.jl")
 include("equilibrium.jl")
+
+
+# plot(map(q -> lbm.plot_quadrature(q, false), [D2Q4(), D2Q5(), D2Q9(), D2Q13(), D2Q17(), D2Q21(), D2Q37()])...)
+
+"""
+Show the arrows of a quadrature, useful for debugging
+"""
+function plot_quadrature(q::Quadrature, color_each_group_separatly = false)
+    unique_weights = unique(q.weights)
+    colors = distinguishable_colors(length(unique_weights))
+
+    limit = max(q.abscissae[1, :]...) + 1
+
+    p = plot(
+        title=string(q),
+        legend=nothing,
+        framestyle=:grid,
+        tickfontcolor=:gray,
+        xlims=(-limit, limit),
+        ylims=(-limit, limit),
+        gridstyle=:dash,
+    )
+
+    for f_idx = 1 : length(q.weights)
+        color_idx = findfirst(isequal(q.weights[f_idx]), unique_weights)
+
+        if q.abscissae[:, f_idx] != [0, 0]
+            quiver!(
+                p,
+                [0.0], [0.0],
+                quiver=(
+                    [q.abscissae[1, f_idx]],
+                    [q.abscissae[2, f_idx]]
+                ),
+                color=color_each_group_separatly ? colors[color_idx] : :black
+            )
+        end
+    end
+
+    for f_idx = 1 : length(q.weights)
+        if q.abscissae[:, f_idx] != [0, 0]
+            continue
+        end
+        color_idx = findfirst(isequal(q.weights[f_idx]), unique_weights)
+        scatter!(
+            p,
+            [(q.abscissae[1, f_idx], q.abscissae[2, f_idx])],
+            # color=colors[color_idx],
+            color=color_each_group_separatly ? colors[color_idx] : :gray,
+            foreground_color_border=:black,
+            markersize=2
+        )
+    end
+
+    return p
+end
