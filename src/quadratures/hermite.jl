0e1a8a53b8072e6da53dc411d980ef08f0b430c9
Mark Redeman <markredeman@gmail.com>
2019-11-04 14:11:17 +0100

    Minor cleanup of hermite.jl

--- src/quadratures/hermite.jl
+++ src/quadratures/hermite.jl
@@ -1,9 +1,13 @@
 # functions to determine hermite coefficients of a function
 
-δ(α, β) = α == β ? 1.0 : 0.0
+δ(α, β) = α == β ? 1 : 0
 hermite(::Type{Val{0}}) = 1.0
 hermite(::Type{Val{0}}, ξ) = hermite(Val{0})
 hermite(::Type{Val{1}}, ξ) = ξ .* hermite(Val{0})
+
+hermite(::Type{Val{0}}, ξ, q) = hermite(Val{0})
+hermite(::Type{Val{1}}, ξ, q) = ξ .* hermite(Val{0})
+
 function hermite(::Type{Val{2}}, ξ)
     D = length(ξ)
     return [
@@ -22,21 +26,83 @@ function hermite(::Type{Val{3}}, ξ)
 end
 
 function hermite(::Type{Val{4}}, ξ)
-    N = 4
     D = length(ξ)
-    H = fill(0.0, D, D, D, D)
-    H_2 = hermite(Val{2}, ξ)
-    H_3 = hermite(Val{3}, ξ)
+    return [
+        ξ[ϵ] * ξ[γ] * ξ[β] * ξ[α] - (
+            ξ[α] * ξ[β] * δ(γ, ϵ) +
+            ξ[α] * ξ[γ] * δ(β, ϵ) +
+            ξ[α] * ξ[ϵ] * δ(β, γ) +
 
-    for i = 1 : D
-        H[i, :, :, :] = ξ[i] * H_3
+            ξ[β] * ξ[γ] * δ(α, ϵ) +
+            ξ[β] * ξ[ϵ] * δ(α, γ) +
+            ξ[γ] * ξ[ϵ] * δ(α, β)
+        ) + (
+            δ(α, β) * δ(γ, ϵ) +
+            δ(α, γ) * δ(β, ϵ) +
+            δ(α, ϵ) * δ(β, γ)
+        )
 
-        for i_1 = 1:D, i_2 = 1:D, i_3 = 1:D
-            H[i, i_1, i_2, i_3] -= δ(i, i_1) * H_2[i_2, i_3] - δ(i, i_2) * H_2[i_1, i_3] - δ(i, i_3) * H_2[i_1, i_2]
-        end
-    end
-    return H
+        for α = 1:D, β = 1:D, γ = 1:D, ϵ = 1:D
+    ]
 end
+
+function hermite(::Type{Val{2}}, ξ, q)
+    D = length(ξ)
+    cs = 1 / q.speed_of_sound_squared
+    return [
+        ξ[β] * ξ[α] - cs * δ(α, β) for α = 1:D, β = 1:D
+    ]
+end
+function hermite(::Type{Val{3}}, ξ, q)
+    D = length(ξ)
+    cs = 1 / q.speed_of_sound_squared
+    return [
+        ξ[γ] * ξ[β] * ξ[α] - cs * (
+            ξ[α] * δ(β, γ) + ξ[β] * δ(α, γ) + ξ[γ] * δ(α, β)
+        )
+
+        for α = 1:D, β = 1:D, γ = 1:D
+    ]
+end
+
+function hermite(::Type{Val{4}}, ξ, q)
+    D = length(ξ)
+    cs = 1 / q.speed_of_sound_squared
+    return [
+        ξ[ϵ] * ξ[γ] * ξ[β] * ξ[α] - cs * (
+            ξ[α] * ξ[β] * δ(γ, ϵ) +
+            ξ[α] * ξ[γ] * δ(β, ϵ) +
+            ξ[α] * ξ[ϵ] * δ(β, γ) +
+
+            ξ[β] * ξ[γ] * δ(α, ϵ) +
+            ξ[β] * ξ[ϵ] * δ(α, γ) +
+            ξ[γ] * ξ[ϵ] * δ(α, β)
+        ) + cs^2 * (
+            δ(α, β) * δ(γ, ϵ) +
+            δ(α, γ) * δ(β, ϵ) +
+            δ(α, ϵ) * δ(β, γ)
+        )
+
+        for α = 1:D, β = 1:D, γ = 1:D, ϵ = 1:D
+    ]
+end
+
+# function hermite(::Type{Val{4}}, ξ)
+#     N = 4
+#     D = length(ξ)
+#     H = fill(0.0, D, D, D, D)
+#     H_2 = hermite(Val{2}, ξ)
+#     H_3 = hermite(Val{3}, ξ)
+
+#     for i = 1 : D
+#         H[i, :, :, :] = ξ[i] * H_3
+
+#         for i_1 = 1:D, i_2 = 1:D, i_3 = 1:D
+#             H[i, i_1, i_2, i_3] -= δ(i, i_1) * H_2[i_2, i_3] - δ(i, i_2) * H_2[i_1, i_3] - δ(i, i_3) * H_2[i_1, i_2]
+#         end
+#     end
+#     return H
+# end
 
 # hermite(::Type{Val{N}}, ξ) where N = begin
 #     D = length(ξ)
