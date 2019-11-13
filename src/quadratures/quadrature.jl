4315ac5ed72e9f5cd1c50fcdeedaec5f46f71517
Mark Redeman <markredeman@gmail.com>
2019-11-13 13:31:33 +0100

    Add a constant including all quadratures

--- src/quadratures/quadrature.jl
+++ src/quadratures/quadrature.jl
@@ -38,6 +38,15 @@ include("D2Q17.jl")
 include("D2Q21.jl")
 include("D2Q37.jl")
 
+const Quadratures = (
+    D2Q4 = D2Q4(),
+    D2Q5 = D2Q5(),
+    D2Q9 = D2Q9(),
+    D2Q13 = D2Q13(),
+    D2Q17 = D2Q17(),
+    D2Q21 = D2Q21(),
+    D2Q37 = D2Q37(),
+)
 
 # plot(map(q -> lbm.plot_quadrature(q, false), [D2Q4(), D2Q5(), D2Q9(), D2Q13(), D2Q17(), D2Q21(), D2Q37()])...)
 
