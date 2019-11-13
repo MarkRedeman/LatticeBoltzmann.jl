8deb0865c19aea56e203b83fa403b5ed62f73e9d
Mark Redeman <markredeman@gmail.com>
2019-11-13 14:29:25 +0100

    Move boundary conditions into subfolder

---
+++ src/boundary-conditions/bounce-back.jl
@@ -0,0 +1,72 @@
+# Halfway Bounce Back rules
+struct BounceBack{D <: Direction, Ints} <: BoundaryCondition
+    direction::D
+    xs::Ints
+    ys::Ints
+end
+
+function apply!(bc::BounceBack{<:North}, q::Quadrature, f_new, f_old)
+    nx, ny, nf = size(f_new)
+    for f_idx = 1:size(f_new, 3)
+        # Bounce back the unkown populations
+        opposite_f_idx = opposite(q, f_idx)
+
+        for y_idx = bc.ys
+            if y_idx + q.abscissae[2, opposite_f_idx] <= ny
+                continue
+            end
+            for x_idx = bc.xs
+                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite_f_idx]
+            end
+        end
+    end
+end
+
+function apply!(bc::BounceBack{<:South}, q::Quadrature, f_new, f_old)
+    nx, ny, nf = size(f_new)
+    for f_idx = 1:size(f_new, 3)
+        opposite_f_idx = opposite(q, f_idx)
+
+        for y_idx = bc.ys
+            if y_idx + q.abscissae[2, opposite_f_idx] > 0
+                continue
+            end
+            for x_idx = bc.xs
+                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite_f_idx]
+            end
+        end
+    end
+end
+
+function apply!(bc::BounceBack{<:East}, q::Quadrature, f_new, f_old)
+    nx, ny, nf = size(f_new)
+    for f_idx = 1:size(f_new, 3)
+        # Bounce back the unkown populations
+        opposite_f_idx = opposite(q, f_idx)
+
+        for x_idx = bc.xs
+            if x_idx + q.abscissae[1, opposite_f_idx] <= nx
+                continue
+            end
+            for y_idx = bc.ys
+                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite_f_idx]
+            end
+        end
+    end
+end
+function apply!(bc::BounceBack{<:West}, q::Quadrature, f_new, f_old)
+    nx, ny, nf = size(f_new)
+    for f_idx = 1:size(f_new, 3)
+        # Bounce back the unkown populations
+        opposite_f_idx = opposite(q, f_idx)
+
+        for x_idx = bc.xs
+            if x_idx + q.abscissae[1, opposite_f_idx] > 0
+                continue
+            end
+            for y_idx = bc.ys
+                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite_f_idx]
+            end
+        end
+    end
+end
---
+++ src/boundary-conditions/moving-wall.jl
@@ -0,0 +1,36 @@
+"""
+The MovingWall boundary condition applies a bounce back rule and adds a velocity
+to each distribution that is bounced back
+"""
+struct MovingWall{D <: Direction, Ints} <: BoundaryCondition
+    direction::D
+    xs::Ints
+    ys::Ints
+    u::Vector{Float64}
+    ρ::Float64
+    T::Float64
+end
+MovingWall(direction, xs, ys, u) = MovingWall(direction, xs, ys, u, 1.0, 1.0)
+
+function apply!(bc::MovingWall{<:North}, q::Quadrature, f_new, f_old)
+    cs = q.speed_of_sound_squared
+
+    a_1_eq = equilibrium_coefficient(Val{1}, q, bc.ρ, bc.u, bc.T)
+    nx, ny, nf = size(f_new)
+    for f_idx = 1:size(f_new, 3)
+        H_i = hermite(Val{1}, q.abscissae[:, f_idx], q)
+        a_1 = q.weights[f_idx] * cs * dot(a_1_eq, H_i)
+
+        opposite_f_idx = opposite(q, f_idx)
+
+        for y_idx = 1:ny
+            if y_idx + q.abscissae[2, opposite_f_idx] <= ny
+                continue
+            end
+            for x_idx = 1:nx
+                f_new[x_idx, y_idx, f_idx] = f_old[x_idx, y_idx, opposite_f_idx] + 2 * a_1
+            end
+        end
+
+    end
+end
