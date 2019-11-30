ac3b1fd04254a4529261e8fb9951c0e5420d1b27
Mark Redeman <markredeman@gmail.com>
2019-11-30 15:57:06 +0100

    Try cleaning up error tracking

--- src/processing-methods/track-hydrodynamic-errors.jl
+++ src/processing-methods/track-hydrodynamic-errors.jl
@@ -63,9 +63,7 @@ function next!(process_method::TrackHydrodynamicErrors, q, f_in, t::Int64)
     Δ = Float64(y_range.step) * Float64(x_range.step)
 
     τ = q.speed_of_sound_squared * lbm.lattice_viscosity(problem) + 0.5
-    # @show q.speed_of_sound_squared * lbm.lattice_viscosity(problem) + 0.5, problem.ν, 1/problem.ν, viscosity(problem)
 
-    # τ = q.speed_of_sound_squared * lbm.lattice_viscosity(problem)
     D = dimension(q)
     N = div(lbm.order(q), 2)
     N = 2
@@ -77,7 +75,7 @@ function next!(process_method::TrackHydrodynamicErrors, q, f_in, t::Int64)
         for n = 1:N
     ]
 
-    @show problem.ν * delta_x(problem)^2 / delta_t(problem)
+    # @show problem.ν * delta_x(problem)^2 / delta_t(problem)
     @inbounds for x_idx in 1:nx, y_idx in 1:ny
         @inbounds for f_idx = 1 : size(f_in, 3)
             f[f_idx] = f_in[x_idx, y_idx, f_idx]
@@ -114,85 +112,35 @@ function next!(process_method::TrackHydrodynamicErrors, q, f_in, t::Int64)
 
 
         # if (x_idx == div(nx, 2) && y_idx == 1)
-            a_f = [
-                sum([f[idx] * Hs[n][idx] for idx = 1:length(q.weights)])
-                for n = 1:N
-            ]
+        a_f = [
+            sum([f[idx] * Hs[n][idx] for idx = 1:length(q.weights)])
+            for n = 1:N
+        ]
+        a_eq_2 = equilibrium_coefficient(Val{2}, q, ρ, a_f[1] ./ ρ, 1.0)
 
-            P = a_f[2] - (a_f[1] * a_f[1]') / ρ - ρ * I(2)
-            P = P * (1 - 1 / (2 * τ))
-            # σ_lb = momentum_flux(q, f, ρ, u) - I(2) * p
-            σ_lb = (P - I(2) * tr(P) / (D))
 
-            # Rescale to dimensionless number (TODO check why problem.u_max)
-            σ_lb = σ_lb / (problem.u_max)
-
-            # P = a_f[2] - (a_f[1] * a_f[1]') / ρ - ρ * I(2)
-            P = (1 - 1 / (2 * τ))a_f[2] - (a_f[1] * a_f[1]') / ρ - ρ * I(2)
-            # P = P * (1 - 1 / (2 * τ))
-            # @show P
-
-        # @show tr(P) / D
-
+        # Rescale to dimensionless number (TODO check why problem.u_max)
+        P = (1 - 1 / (2 * τ))a_f[2] - (a_f[1] * a_f[1]') / ρ - ρ * I
         error_p += Δ * ((-tr(P)/D) - expected_p)^2
 
-            σ_lb = (P - I(2) * tr(P) / (D)) / (problem.u_max)
-
-            a_eq_2 = equilibrium_coefficient(Val{2}, q, ρ, a_f[1] ./ ρ, 1.0)
-            σ_a = (a_f[2] - a_eq_2) / (1 - 1 / (2 * τ))
-
-            a_2 = (a_f[2] + (1 / (2 * τ)) * a_eq_2) / (1 + 1 / (2 * τ))
-
-        # Vorige poging
-        # @show "SIGMAS"
-            σ_lb = (a_2 - (a_f[1] * a_f[1]') / ρ) / problem.u_max
-            # @show σ_lb
+        a_2 = (a_f[2] + (1 / (2 * τ)) * a_eq_2) / (1 + 1 / (2 * τ))
 
         # Huidige poging
-            P = a_2 - (a_f[1] * a_f[1]') / ρ - ρ * I
-            σ_lb = (P - (1/D) * tr(P) * I)
-            # @show σ_lb
-            # @show P -(P - (1/D) * tr(P) * I)
+        P = a_2 - (a_f[1] * a_f[1]') / ρ - ρ * I
+        σ_lb = (P - (1/D) * tr(P) * I)
 
-            # @show (a_f[1] * a_f[1]') / ρ
-            # @show σ_a
-            # @show (σ_a - (a_f[1] * a_f[1]') / ρ)
-            # @show (σ_a - (a_f[1] * a_f[1]') / ρ) / problem.u_max
-            # @show a_f[2] a_eq_2 σ_a / problem.u_max
-            # @show tr(σ_a)
-
-            factor = 0.9549296889427464
-        # factor = q.speed_of_sound_squared / pi
-            factor = (1.0 / delta_x(problem)) / 16
-            factor = (1.0 / (delta_t(problem)))
-        # factor *= 6/8
-        # factor *= 6/8
-        factor *= 3/4
-            σ_lb *= (factor)
-            cs = 1 / (q.speed_of_sound_squared)
-            σ_exact = deviatoric_tensor(q, problem, x, y, time)
-            # σ_exact /= (problem.domain_size[1] / delta_x(problem)) / 16
-
-        # P = (
-        #     a_f[2] - ρ * (u * u' - I)
-        # )
-        # @show P P /(1 + 1 / (2 * τ)) P / problem.u_max P / (problem.u_max * (1 + 1 / (2 * τ))) σ_lb σ_exact
-
-            σ_err = (σ_exact .- σ_lb)
-        # @show σ_err
-            # @show x y
-            # @show σ_lb σ_exact σ_err
-            # @show σ_exact ./ σ_lb
-            # @show σ_lb ./ σ_exact
-            # @show σ_exact[1,2] σ_lb[1,2]
-            # @show Δ *(σ_exact .- σ_lb).^2
+        factor = delta_x(problem)^2 / delta_t(problem)^2 * 3/4
+        # factor *= 0.964234137002314
+        σ_lb *= (factor)
+        σ_exact = deviatoric_tensor(q, problem, x, y, time)
+        σ_err = (σ_exact .- σ_lb)
+       
         if (x_idx == div(nx, 2) && y_idx == 1)
-            @show factor
+            # @show σ_exact σ_lb σ_err
             factor =  σ_exact[1,2] ./ σ_lb[1,2]
-            @show factor
+            @show  σ_exact[1,2] ./ σ_lb[1,2]
             factor =  σ_exact[2,2] ./ σ_lb[2,2]
-            @show factor
-            @show σ_err
+            # @show  σ_exact[2,2] ./ σ_lb[2,2]
             # @show τ q.speed_of_sound_squared
             # @show pi * τ, pi * q.speed_of_sound_squared, factor
             # @show (1.0 / delta_x(problem)) / 16
@@ -206,7 +154,7 @@ function next!(process_method::TrackHydrodynamicErrors, q, f_in, t::Int64)
             # @show factor / problem.ν
 
             # factor =  σ_exact[1,2] ./ σ_lb[1,2]
-            factor =  σ_exact[2,2] ./ σ_lb[2,2]
+            # factor =  σ_exact[2,2] ./ σ_lb[2,2]
             # @show factor σ_lb σ_exact (σ_exact .- σ_lb).^2
             # @show factor * q.speed_of_sound_squared
             # @show factor / q.speed_of_sound_squared
@@ -216,19 +164,18 @@ function next!(process_method::TrackHydrodynamicErrors, q, f_in, t::Int64)
             # @show factor / τ
             # @show factor * problem.ν
             # @show factor / problem.ν
-            end
+        end
 
-            error_σ_xx += Δ * σ_err[1, 1]^2
-            error_σ_xy += Δ * σ_err[1, 2]^2
-            error_σ_yx += Δ * σ_err[2, 1]^2
-            error_σ_yy += Δ * σ_err[2, 2]^2
-        # end
+        error_σ_xx += Δ * σ_err[1, 1]^2
+        error_σ_xy += Δ * σ_err[1, 2]^2
+        error_σ_yx += Δ * σ_err[2, 1]^2
+        error_σ_yy += Δ * σ_err[2, 2]^2
     end
 
-    @show error_σ_xx
-    @show error_σ_xy
-    @show sqrt(error_σ_xx)
-    @show sqrt(error_σ_xy)
+    # @show error_σ_xx
+    # @show error_σ_xy
+    # @show sqrt(error_σ_xx)
+    # @show sqrt(error_σ_xy)
 
     # @show error_σ_xy
     push!(process_method.df, (
