aad22c553fbbe5fdc995664ed21e70851630542d
Mark Redeman <markredeman@gmail.com>
2020-02-12 23:44:59 +0100

    Add StaticArrays variants of moments benchmarks
    In the future I would like to replace all internal variables with
    mutable static arrays, which I assume will give some nice speedups.

--- benchmark/bench_moments.jl
+++ benchmark/bench_moments.jl
@@ -2,6 +2,7 @@ module BenchMoments
 
 using BenchmarkTools
 using LatticeBoltzmann
+using StaticArrays
 
 import LatticeBoltzmann:
     density,
@@ -35,6 +36,34 @@ for q = [D2Q9(), D2Q13(), D2Q17(), D2Q21(), D2Q37()]
                 f = copy($q.weights);
                 ρ = density($q, f);
                 u = zeros(eltype(f), dimension($q))
+            )
+        )
+
+
+    suite[string(q)]["density", "static"] =
+        @benchmarkable(
+            density(static_q, f),
+            setup = (
+                f = $q.weights |> MVector{length($q.weights)};
+                static_q = D2Q9(
+                    $q.abscissae |> SMatrix{dimension($q), length($q.weights)},
+                    tuple($q.weights...) |> SVector{length($q.weights)},
+                    $q.speed_of_sound_squared
+                )
+            )
+        )
+    suite[string(q)]["velocity", "static"] =
+        @benchmarkable(
+            velocity!(static_q, f, ρ, u),
+            setup = (
+                f = $q.weights |> MVector{length($q.weights)};
+                ρ = density($q, f);
+                u = @MVector zeros(eltype(f), dimension($q));
+                static_q = D2Q9(
+                    $q.abscissae |> SMatrix{dimension($q), length($q.weights)},
+                    tuple($q.weights...) |> SVector{length($q.weights)},
+                    $q.speed_of_sound_squared
+                )
             )
         )
 end
