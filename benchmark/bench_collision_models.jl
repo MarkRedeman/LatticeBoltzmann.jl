7dfbd496c919023b18063ad7d3b90669f7178eb6
Mark Redeman <markredeman@gmail.com>
2020-02-14 07:34:18 +0000

    Format files using JuliaFormatter

--- benchmark/bench_collision_models.jl
+++ benchmark/bench_collision_models.jl
@@ -8,10 +8,14 @@ suite = BenchmarkGroup()
 function initialize_benchmark(q = D2Q9(), τ = 1.0, scale = 2)
     benchmark_problem = LatticeBoltzmann.TGV(q, τ, scale)
 
-    LatticeBoltzmann.initialize(LatticeBoltzmann.ZeroVelocityInitialCondition(), q, benchmark_problem)
+    LatticeBoltzmann.initialize(
+        LatticeBoltzmann.ZeroVelocityInitialCondition(),
+        q,
+        benchmark_problem,
+    )
 end
 
-for q = [D2Q9(), D2Q13(), D2Q17(), D2Q21(), D2Q37()]
+for q in [D2Q9(), D2Q13(), D2Q17(), D2Q21(), D2Q37()]
     suite[string(q)] = BenchmarkGroup([string(q), "collision_models"])
 
     srt_equilibrium = SRT(1.0)
@@ -21,64 +25,60 @@ for q = [D2Q9(), D2Q13(), D2Q17(), D2Q21(), D2Q37()]
     τ = 1.0
     f = initialize_benchmark(q, τ)
 
-    suite[string(q)]["srt", 1.0] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (cm = $srt_equilibrium; f_in = copy($f); f_out = copy($f))
-        )
+    suite[string(q)]["srt", 1.0] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = $srt_equilibrium; f_in = copy($f); f_out = copy($f))
+    )
 
-    suite[string(q)]["srt", 1.3] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (cm = SRT(1.3); f_in = copy($f); f_out = copy($f))
-        )
+    suite[string(q)]["srt", 1.3] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = SRT(1.3); f_in = copy($f); f_out = copy($f))
+    )
 
-    suite[string(q)]["srt", 0.8] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (cm = SRT(0.8); f_in = copy($f); f_out = copy($f))
-        )
+    suite[string(q)]["srt", 0.8] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = SRT(0.8); f_in = copy($f); f_out = copy($f))
+    )
 
-    suite[string(q)]["trt", (1.0, 1.0)] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (cm = $trt_equilibrium; f_in = copy($f); f_out = copy($f))
-        )
+    suite[string(q)]["trt", (1.0, 1.0)] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = $trt_equilibrium; f_in = copy($f); f_out = copy($f))
+    )
 
-    suite[string(q)]["trt", (0.9, 1.1)] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (cm = TRT(0.9, 1.1); f_in = copy($f); f_out = copy($f))
-        )
+    suite[string(q)]["trt", (0.9, 1.1)] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = TRT(0.9, 1.1); f_in = copy($f); f_out = copy($f))
+    )
 
-    suite[string(q)]["mrt-equilibrium", 1.0] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (cm = $mrt_equilibrium; f_in = copy($f); f_out = copy($f))
-        )
+    suite[string(q)]["mrt-equilibrium", 1.0] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = $mrt_equilibrium; f_in = copy($f); f_out = copy($f))
+    )
 
     scale = 2
-    benchmark_problem = LatticeBoltzmann.DecayingShearFlow(1.0 / (2 * q.speed_of_sound_squared), scale, static = true)
-    f_force = LatticeBoltzmann.initialize(LatticeBoltzmann.ZeroVelocityInitialCondition(), q, benchmark_problem)
-    suite[string(q)]["srt", "force"] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (
-                cm = CollisionModel(SRT, $q, $benchmark_problem);
-                f_in = copy($f_force);
-                f_out = copy($f_force)
-            )
-        )
+    benchmark_problem = LatticeBoltzmann.DecayingShearFlow(
+        1.0 / (2 * q.speed_of_sound_squared),
+        scale,
+        static = true,
+    )
+    f_force = LatticeBoltzmann.initialize(
+        LatticeBoltzmann.ZeroVelocityInitialCondition(),
+        q,
+        benchmark_problem,
+    )
+    suite[string(q)]["srt", "force"] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = CollisionModel(SRT, $q, $benchmark_problem);
+        f_in = copy($f_force);
+        f_out = copy($f_force))
+    )
 
-    suite[string(q)]["trt", "force"] =
-        @benchmarkable(
-            collide!(cm, $q, f_in, f_out),
-            setup = (
-                cm = CollisionModel(TRT, $q, $benchmark_problem);
-                f_in = copy($f_force);
-                f_out = copy($f_force)
-            )
-        )
+    suite[string(q)]["trt", "force"] = @benchmarkable(
+        collide!(cm, $q, f_in, f_out),
+        setup = (cm = CollisionModel(TRT, $q, $benchmark_problem);
+        f_in = copy($f_force);
+        f_out = copy($f_force))
+    )
 end
 
 end  # module
--- benchmark/bench_moments.jl
+++ benchmark/bench_moments.jl
@@ -5,105 +5,80 @@ using LatticeBoltzmann
 using StaticArrays
 
 import LatticeBoltzmann:
-    density,
-    velocity!,
-    temperature,
-    pressure,
-    momentum_flux,
-    deviatoric_tensor
+    density, velocity!, temperature, pressure, momentum_flux, deviatoric_tensor
 
 suite = BenchmarkGroup()
 
-for q = LatticeBoltzmann.Quadratures
+for q in LatticeBoltzmann.Quadratures
     suite[string(q)] = BenchmarkGroup([string(q), "moments"])
 
     suite[string(q)]["density"] =
-        @benchmarkable(
-            density($q, f),
-            setup = (f = copy($q.weights);)
-        )
+        @benchmarkable(density($q, f), setup = (f = copy($q.weights)))
 
-    suite[string(q)]["velocity"] =
-        @benchmarkable(
-            velocity!($q, f, ρ, u),
-            setup = (
-                f = copy($q.weights);
-                ρ = density($q, f);
-                u = zeros(eltype(f), dimension($q))
-            )
-        )
+    suite[string(q)]["velocity"] = @benchmarkable(
+        velocity!($q, f, ρ, u),
+        setup = (f = copy($q.weights);
+        ρ = density($q, f);
+        u = zeros(eltype(f), dimension($q)))
+    )
 
-    suite[string(q)]["pressure"] =
-        @benchmarkable(
-            pressure($q, f, ρ, u),
-            setup = (
-                f = copy($q.weights);
-                ρ = density($q, f);
-                u = zeros(eltype(f), dimension($q));
-                velocity!($q, f, ρ, u)
-            )
-        )
+    suite[string(q)]["pressure"] = @benchmarkable(
+        pressure($q, f, ρ, u),
+        setup = (f = copy($q.weights);
+        ρ = density($q, f);
+        u = zeros(eltype(f), dimension($q));
+        velocity!($q, f, ρ, u))
+    )
 
-    suite[string(q)]["temperature"] =
-        @benchmarkable(
-            pressure($q, f, ρ, u),
-            setup = (
-                f = copy($q.weights);
-                ρ = density($q, f);
-                u = zeros(eltype(f), dimension($q));
-                velocity!($q, f, ρ, u)
-            )
-        )
+    suite[string(q)]["temperature"] = @benchmarkable(
+        pressure($q, f, ρ, u),
+        setup = (f = copy($q.weights);
+        ρ = density($q, f);
+        u = zeros(eltype(f), dimension($q));
+        velocity!($q, f, ρ, u))
+    )
 
-    suite[string(q)]["momentum_flux"] =
-        @benchmarkable(
-            momentum_flux($q, f, ρ, u),
-            setup = (
-                f = copy($q.weights);
-                ρ = density($q, f);
-                u = zeros(eltype(f), dimension($q));
-                velocity!($q, f, ρ, u)
-            )
-        )
+    suite[string(q)]["momentum_flux"] = @benchmarkable(
+        momentum_flux($q, f, ρ, u),
+        setup = (f = copy($q.weights);
+        ρ = density($q, f);
+        u = zeros(eltype(f), dimension($q));
+        velocity!($q, f, ρ, u))
+    )
 
-    suite[string(q)]["deviatoric_tensor"] =
-        @benchmarkable(
-            deviatoric_tensor($q, τ, f, ρ, u),
-            setup = (
-                τ = 1.0;
-                f = copy($q.weights);
-                ρ = density($q, f);
-                u = zeros(eltype(f), dimension($q));
-                velocity!($q, f, ρ, u)
-            )
-        )
+    suite[string(q)]["deviatoric_tensor"] = @benchmarkable(
+        deviatoric_tensor($q, τ, f, ρ, u),
+        setup = (τ = 1.0;
+        f = copy($q.weights);
+        ρ = density($q, f);
+        u = zeros(eltype(f), dimension($q));
+        velocity!($q, f, ρ, u))
+    )
 
-    suite[string(q)]["density", "static"] =
-        @benchmarkable(
-            density(static_q, f),
-            setup = (
-                f = $q.weights |> MVector{length($q.weights)};
-                static_q = D2Q9(
-                    $q.abscissae |> SMatrix{dimension($q), length($q.weights)},
-                    tuple($q.weights...) |> SVector{length($q.weights)},
-                    $q.speed_of_sound_squared
-                )
+    suite[string(q)]["density", "static"] = @benchmarkable(
+        density(static_q, f),
+        setup = (
+            f = $q.weights |> MVector{length($q.weights)};
+            static_q = D2Q9(
+                $q.abscissae |> SMatrix{dimension($q), length($q.weights)},
+                tuple($q.weights...) |> SVector{length($q.weights)},
+                $q.speed_of_sound_squared,
             )
         )
-    suite[string(q)]["velocity", "static"] =
-        @benchmarkable(
-            velocity!(static_q, f, ρ, u),
-            setup = (
-                f = $q.weights |> MVector{length($q.weights)};
-                ρ = density($q, f);
-                u = @MVector zeros(eltype(f), dimension($q));
-                static_q = D2Q9(
-                    $q.abscissae |> SMatrix{dimension($q), length($q.weights)},
-                    tuple($q.weights...) |> SVector{length($q.weights)},
-                    $q.speed_of_sound_squared
-                )
+    )
+    suite[string(q)]["velocity", "static"] = @benchmarkable(
+        velocity!(static_q, f, ρ, u),
+        setup = (
+            f = $q.weights |> MVector{length($q.weights)};
+            ρ = density($q, f);
+            u = @MVector zeros(eltype(f), dimension($q));
+            static_q = D2Q9(
+                $q.abscissae |> SMatrix{dimension($q), length($q.weights)},
+                tuple($q.weights...) |> SVector{length($q.weights)},
+                $q.speed_of_sound_squared,
             )
         )
+    )
 end
 
 end  # module
